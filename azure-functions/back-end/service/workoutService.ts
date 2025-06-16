f2aca3eecaecf303af2ad08ae140873355cb0c82
deklercknathan <r0925964@ucll.be>
2025-06-16 19:14:56 +0200

    sorry als alles kapot is hihi

---
+++ azure-functions/back-end/service/workoutService.ts
@@ -0,0 +1,40 @@
+import workoutRepository from '../repository/workout.db';
+import userRepository from '../repository/user.db';
+import typeRepository from '../repository/type.db';
+import { WorkoutInput } from '../types';
+
+const getAllWorkouts = async () => {
+    return await workoutRepository.getAllWorkouts();
+};
+
+const getWorkoutByUser = async (email: string) => {
+    const user = await userRepository.getUserByEmail(email);
+    if (!user) {
+        throw new Error('There is no user with that email address.');
+    }
+    return await workoutRepository.getWorkoutByUser(user.id);
+};
+
+const createWorkout = async ({ title, date, type: typeInput, user: userInput }: WorkoutInput) => {
+    const type = await typeRepository.getTypeById(typeInput.title);
+    if (!type) {
+        throw new Error('There is no type like that');
+    }
+    const user = await userRepository.getUserByEmail(userInput.email);
+    if (!user) {
+        throw new Error('There is no user like that');
+    }
+    const workoutData = {
+        title,
+        date,
+        type: type.id,
+        user: user.id,
+    };
+    return await workoutRepository.createWorkout(workoutData);
+};
+
+export default {
+    getAllWorkouts,
+    getWorkoutByUser,
+    createWorkout,
+};

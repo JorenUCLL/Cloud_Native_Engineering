f6ab0b53af24039b132ed0a004c824559ef028ac
JorenUCLL <joren.vanlaer@student.ucll.be>
2025-06-16 18:27:32 +0200

    plop

--- back-end/app.ts
+++ back-end/app.ts
@@ -36,7 +36,7 @@ mongoose
 
 app.use(
     cors({
-        origin: 'cloud-native-engineering-afa3a4cpd5dxexfq.westeurope-01.azurewebsites.net',
+        origin: 'http://localhost:8081',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true,
     })

afcff6110cd0b667a9154fe6857c8b0624e9740e
deklercknathan <r0925964@ucll.be>
2025-06-17 04:40:38 +0200

    test

--- azure-functions/src/getUserByEmailHttpTrigger/index.ts
+++ azure-functions/src/getUserByEmailHttpTrigger/index.ts
@@ -1,12 +1,9 @@
 const userService = require("../../back-end/service/userService");
 
-module.exports = async function (context, req) {
+module.exports = async function getUserByEmailHttpTrigger(context, req) {
   context.log("test Function triggered successfully.");
 
   const email = req.params.email || (req.body && req.body.email);
-
-  // You can use userService here to fetch user by email, example:
-  // const user = await userService.getUserByEmail(email);
 
   context.res = {
     status: 200,

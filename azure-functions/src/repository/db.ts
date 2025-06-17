4afef64946aaff48e58ac16f236c48d450d10a29
JorenUCLL <joren.vanlaer@student.ucll.be>
2025-06-17 17:51:29 +0200

    tering

--- azure-functions/src/repository/db.ts
+++ azure-functions/src/repository/db.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log("testing connection to MongoDB");
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected");
   } catch (error) {

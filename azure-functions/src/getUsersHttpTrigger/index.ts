0a86b24143dc005f59b0dbdbe891e5953a6e34f5
Niel Verhoeven <niel.verhoeven@student.ucll.be>
2025-06-17 11:42:48 +0200

    getUsersHttpTrigger

---
+++ azure-functions/src/getUsersHttpTrigger/index.ts
@@ -0,0 +1,21 @@
+const userService = require("../../back-end/service/userService");
+
+module.exports = async function (context, req) {
+  try {
+    context.log("getUsers function triggered.");
+
+    const users = await userService.getAllUsers();
+
+    context.res = {
+      status: 200,
+      body: users,
+      headers: { "Content-Type": "application/json" },
+    };
+  } catch (err) {
+    context.log.error("Error in getUsers function:", err);
+    context.res = {
+      status: 500,
+      body: { error: "Internal server error" },
+    };
+  }
+};

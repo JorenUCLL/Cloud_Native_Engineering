f6baba83046b6539243627040079590c3d99222b
Niel Verhoeven <niel.verhoeven@student.ucll.be>
2025-06-19 00:00:19 +0200

    user in createWorkout

--- front-end/components/workouts/createWorkout.tsx
+++ front-end/components/workouts/createWorkout.tsx
@@ -24,20 +24,11 @@ const WorkoutModal: React.FC<WorkoutModalProps> = ({ onClose, onCreate }) => {
   const [exerciseInput, setExerciseInput] = useState("");
   const [allTypes, setAllTypes] = useState<Type[]>([]);
   const [selectedExercise, setSelectedExercise] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [token, setToken] = useState<string>("");
 
   const [allExercises, setAllExercises] = useState<Exercise[]>([]);
 
   useEffect(() => {
-    const loggedInUser = sessionStorage.getItem("loggedInUser");
-    if (!loggedInUser) {
-      console.error("No user logged in");
-      return;
-    }
-    const loggedInData = JSON.parse(loggedInUser);
-    setEmail(loggedInData.email);
-    setToken(loggedInData.token);
+    
 
     (async () => {
       try {
@@ -66,9 +57,17 @@ const WorkoutModal: React.FC<WorkoutModalProps> = ({ onClose, onCreate }) => {
   };
 
   const handleCreate = async () => {
+    const loggedInUser = sessionStorage.getItem("loggedInUser");
+    if (!loggedInUser) {
+      console.error("No user logged in");
+      return;
+    }
+    const loggedInData = JSON.parse(loggedInUser);
+    const email = (loggedInData.email);
+    const token = (loggedInData.token);
     const user = await UserService.getUserByEmail(
       email, token);
-      
+
     console.log("Creating workout with data:",
       {
         title,
